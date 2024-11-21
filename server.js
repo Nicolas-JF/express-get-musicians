9ffc88a130c63f618d73fa9ea29788cc7104d8e6
Nicolas Fabius <nicolas.fabius@nicolas.fabius-MacBook-Pro>
2024-11-21 16:36:34 -0500

    Server.js roll back and updated index.test.js to pass test

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const musiciansRouter = require('./src/routes/musicians');
+const app = require("./src/app");
+const { db } = require("./db/connection")
+const port = 3001;
 
-const app = express();
-
-app.use(bodyParser.json());
-app.use('/api/musicians', musiciansRouter);
-
-mongoose.connect('mongodb://localhost:27017/musicianDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+app.listen(port, () => {
+    db.sync();
+    console.log(`Listening at http://localhost:${port}/musicians`)
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.log('Error connecting to MongoDB: ', err));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
