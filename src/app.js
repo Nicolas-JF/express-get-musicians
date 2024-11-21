cd4621c9f1ebf911ee2bb8fd591a05e670a8d92b
Nicolas Fabius <nicolas.fabius@nicolas.fabius-MacBook-Pro>
2024-11-21 12:47:02 -0500

    Updated CRUD operations for musicians in app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const { Musician } = require("../models");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/musicians/:id", async (req, res) => {
     const id = req.params.id;
@@ -18,5 +19,54 @@ app.get("/musicians/:id", async (req, res) => {
     }
 });
 
+app.post("/musicians", async (req, res) => {
+    const { name, instrument } = req.body;
+    try {
+        const newMusician = await Musician.create({ name, instrument });
+        res.status(201).json(newMusician);
+    } catch (error) {
+        res.status(500).json({ message: "Error creating musician", error });
+    }
+});
+
+app.put("/musicians/:id", async (req, res) => {
+    const { id } = req.params;
+    const { name, instrument } = req.body;
+    try {
+        const musician = await Musician.findByPk(id);
+        if (musician) {
+            musician.name = name;
+            musician.instrument = instrument;
+            await musician.save();
+            res.json(musician);
+        } else {
+            res.status(404).json({ message: "Musician not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error updating musician", error });
+    }
+});
+
+app.delete("/musicians/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const musician = await Musician.findByPk(id);
+        if (musician) {
+            await musician.destroy();
+            res.status(204).send();
+        } else {
+            res.status(404).json({ message: "Musician not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting musician", error });
+    }
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
 module.exports = app;
+
 
