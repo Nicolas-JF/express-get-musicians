cb7c47fdff62010febfc5e2fdc93dd4b9cd9b2b7
Nicolas Fabius <nicolas.fabius@nicolas.fabius-MacBook-Pro>
2024-11-21 14:40:32 -0500

    Updating server.js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const app = require("./src/app");
-const { db } = require("./db/connection")
-const port = 3001;
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const musiciansRouter = require('./routes/musicians');
 
-app.listen(port, () => {
-    db.sync();
-    console.log(`Listening at http://localhost:${port}/musicians`)
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/api/musicians', musiciansRouter);
+
+mongoose.connect('mongodb://localhost:27017/musicianDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.log('Error connecting to MongoDB: ', err));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
